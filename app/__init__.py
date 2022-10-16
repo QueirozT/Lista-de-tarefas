5d316e451eb21708941008b3340a4722b7a6f18d
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-10-16 20:20:31 -0300

    Atualizando o factor do projeto e configurando as extensões

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,31 @@
 from flask import Flask
+from flask_babel import Babel
+from flask_migrate import Migrate
 from pathlib import Path
 
 from .auth import bp_auth
 from .tarefas import bp_tarefas
+from .models import config as config_db
+from .serializer import config as config_ma
 
 BASE_DIR = Path(__file__).parent.parent
 
+migrate = Migrate()
+babel = Babel()
 
 def create_app(Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    babel.init_app(app)
+
+    config_db(app)
+    config_ma(app)
+    migrate.init_app(app, app.db)
+
     app.template_folder = Path.joinpath(BASE_DIR, 'resources', 'template')
     app.static_folder = Path.joinpath(BASE_DIR, 'resources', 'static')
-    
+
     app.register_blueprint(bp_auth)
     app.register_blueprint(bp_tarefas)
     
