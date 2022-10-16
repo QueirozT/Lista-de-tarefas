0132ec100fafc2aa18c96d0d2d41ad026c49e70e
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-10-15 21:52:33 -0300

    Criando o factory do projeto
    Dentro do factory estou definindo o caminho para os templates, para os estáticos e registrando as blueprints.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
-from config import Config
+from pathlib import Path
+
+from .auth import bp_auth
+from .tarefas import bp_tarefas
+
+BASE_DIR = Path(__file__).parent.parent
 
 
 def create_app(Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    app.template_folder = Path.joinpath(BASE_DIR, 'resources', 'template')
+    app.static_folder = Path.joinpath(BASE_DIR, 'resources', 'static')
     
+    app.register_blueprint(bp_auth)
+    app.register_blueprint(bp_tarefas)
     
     return app
