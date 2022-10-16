50b826708068f8ad89e639c96fc3649dbff11644
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-10-16 20:18:07 -0300

    Atualizando o wsgi,py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,15 @@
 from app import create_app
 from config import ProdConf
 
+from app.models import User, Tarefas
+
 
 app = create_app(ProdConf)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': app.db, 'User': User, 'Tarefas': Tarefas}
 
 
 if __name__ == '__main__':
