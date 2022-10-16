e6982a693965e6c64966e990355ab6ceb6196010
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-10-16 20:18:57 -0300

    Criando um serializer para o modelo das tarefas

---
+++ app/serializer.py
@@ -0,0 +1,25 @@
+from marshmallow import fields, validates, ValidationError
+from flask_marshmallow import Marshmallow
+
+from .models import Tarefas
+
+ma = Marshmallow()
+
+def config(app):
+    ma.init_app(app)
+
+
+class TarefaSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Tarefas
+        load_instance = True
+
+    title = fields.Str(required=True)
+    description = fields.Str(required=True)
+    type = fields.Str(required=True)
+    priority = fields.Bool(required=True)
+    user_id = fields.Int(required=True)
+
+    @validates('id')
+    def validate_id(self, value):
+        raise ValidationError('Não envia o campo ID.')
