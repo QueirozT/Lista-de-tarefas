ae9c80d7f0256e2fb84819ad1bc4e23317f4bda9
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-10-16 20:18:28 -0300

    Atualizando as configurações do projeto

--- config.py
+++ config.py
@@ -2,9 +2,15 @@ from decouple import config
 
 class ProdConf(object):
     DEBUG = False
+    BABEL_DEFAULT_LOCALE ='pt_BR'
     SECRET_KEY = config('SECRET_KEY', default="você-nunca-vai-adivinhar")
+    SQLALCHEMY_DATABASE_URI = config('DATABASE_URI', default='sqlite:///')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestConf(object):
     TESTING = True
+    WTF_CSRF_ENABLED = False
     SECRET_KEY = "você-nunca-vai-adivinhar"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
