06f6f5e78902d64677c10d474f191fd27e0aab52
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-10-15 22:35:03 -0300

    Atualizando as rotas

--- app/auth.py
+++ app/auth.py
@@ -1,19 +1,26 @@
-from crypt import methods
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint, redirect, render_template, request, url_for
+)
+from flask_login import current_user
 
 bp_auth = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @bp_auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    return render_template('login.html', current_user=current_user)
+
+
+@bp_auth.route('/logout')
+def logout():
+    return redirect(url_for('auth.login'))
 
 
 @bp_auth.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    return render_template('register.html', current_user=current_user)
 
 
 @bp_auth.route('/profile', methods=['GET', 'POST'])
 def profile():
-    return render_template('perfil.html')
+    return render_template('perfil.html', current_user=current_user)
--- app/tarefas.py
+++ app/tarefas.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 bp_tarefas = Blueprint('tarefas', __name__)
 
@@ -6,4 +7,4 @@ bp_tarefas = Blueprint('tarefas', __name__)
 @bp_tarefas.route('/')
 @bp_tarefas.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_user=current_user)
