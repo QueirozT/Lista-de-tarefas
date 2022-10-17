17c6c6ed017ac097f7827789eb28b1fbc6a7b4ed
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-10-17 18:27:41 -0300

    Atualizando o script do projeto
    Estou fazendo a comunicação entre o front e o back através de requisições para a API do projeto.

--- resources/static/js/script.js
+++ resources/static/js/script.js
@@ -3,6 +3,8 @@ const LISTA = document.querySelector('#todo')
 const FAZER = document.querySelector('#doing')
 const FEITO = document.querySelector('#done')
 
+
+
 // Modelos
 function InserirModelo(id, header, description, type, priority) {
     div = document.createElement('div')
@@ -114,114 +116,226 @@ function InserirModelo(id, header, description, type, priority) {
     }
 }
 
-/* COMANDOS USADOS PARA GERAR OS 3 TIPOS DE LISTA:
 
-InserirModelo(1, 'Modelo Lista', 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 'lista')
-
-InserirModelo(1, 'Modelo Fazer', 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 'fazer')
-
-InserirModelo(1, 'Modelo Feito', 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 'feito')
-*/
 
 // Funções
-var cont = 5
-function CriarTarefa() {
-    InserirModelo(
-        cont,
-        document.querySelector('#title').value,
-        document.querySelector('#descricao').value,
-        'lista',
-        document.querySelector('#priority').checked
-    )
-    cont++
+function ListarTarefas() {
+    fetch(location.origin + "/collect", {method: "GET"})
+        .then((response) => response.json())
+        .then((data) => {
+    
+            for (obj in data) {
+                InserirModelo(
+                    data[obj].id,
+                    data[obj].title,
+                    data[obj].description,
+                    data[obj].type,
+                    data[obj].priority
+                )
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+    });
 }
 
 function FazerTarefa(obj) {
-    id = obj.id
-    title = obj.children[0].innerText
-    description = obj.children[1].innerText
-    priority = obj.classList.contains('priority')
 
-    InserirModelo(id, title, description, 'fazer', priority)
-    obj.remove()
+    fetch(location.origin + "/update/" + obj.id, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({'type': 'fazer'}),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+
+            InserirModelo(
+                data.id, 
+                data.title, 
+                data.description, 
+                data.type,
+                data.priority
+            )
+            obj.remove()
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+    });
 }
 
 function RefazerTarefa(obj) {
-    id = obj.id
-    title = obj.children[0].innerText
-    description = obj.children[1].innerText
-    priority = obj.classList.contains('priority')
 
-    InserirModelo(id, title, description, 'lista')
-    obj.remove()
+    fetch(location.origin + "/update/" + obj.id, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({'type': 'lista'}),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+
+            InserirModelo(
+                data.id, 
+                data.title, 
+                data.description, 
+                data.type,
+                data.priority
+            )
+            obj.remove()
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+    });
 }
 
 function ConcluirTarefa(obj) {
-    id = obj.id
-    title = obj.children[0].innerText
-    description = obj.children[1].innerText
-    InserirModelo(id, title, description, 'feito')
-    obj.remove()
+
+    fetch(location.origin + "/update/" + obj.id, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({'type': 'feito'}),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+
+            InserirModelo(
+                data.id, 
+                data.title, 
+                data.description, 
+                data.type,
+                false
+            )
+            obj.remove()
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+    });
+}
+
+function RemoverTarefa(obj) {
+
+    fetch(location.origin + "/delete/" + obj.id, {method: "DELETE"})
+        .then((response) => response.data)
+        .then((data) => {
+
+            obj.remove()
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+    });
 }
 
 
 // Gatilhos
+ListarTarefas()
+
 document.querySelector('#submit').addEventListener('click', (e) => {
-    CriarTarefa()
+    // CRIAR A TAREFA COM O RESPONSE DO POST
+    title = document.querySelector('#title')
+    description = document.querySelector('#descricao')
+    priority = document.querySelector('#priority')
+    msg = "Este campo não pode ficar vazio"
+
+    if (title.validity.valueMissing | description.validity.valueMissing) {
+        if (title.validity.valueMissing & description.validity.valueMissing) {
+            title.placeholder = msg
+            description.placeholder = msg
+        }
+        else {
+            if (title.validity.valueMissing) {
+                title.placeholder = msg
+            }
+            else {
+                description.placeholder = msg
+            }
+        }
+        return
+    }
+
+    // #######################
+
+    const data = { 
+        'title': title.value, 
+        'description': description.value,
+        'type': 'lista',
+        'priority': priority.checked
+    };
+
+    fetch(location.origin + "/create", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        
+    InserirModelo(
+        data.id,
+        data.title,
+        data.description,
+        data.type,
+        data.priority
+    )
+
+    title.value = ''
+    title.placeholder = ''
+    description.value = ''
+    description.placeholder = ''
+    priority.checked = false
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
 })
 
 document.querySelector('#todo').addEventListener('click', function(e) {
-    id = e.target.parentNode.parentNode.getAttribute('id')
     obj = e.target.parentNode.parentNode
 
-    console.log('ID = ', id)
-    console.log('OBJ = ', obj)
-
     if (e.target.classList.contains('btn-fazer')) {
-        console.log('botão Fazer')
 
+        // ATUALIZAR O TYPE PARA "FAZER"
         FazerTarefa(obj)
     }
     if (e.target.classList.contains('btn-cancelar')) {
-        console.log('botão Cancelar')
-        obj.remove()
+
+        // REMOVER O OBJETO DO BANCO DE DADOS
+        RemoverTarefa(obj)
     }
 });
 
 document.querySelector('#doing').addEventListener('click', function(e) {
-    id = e.target.parentNode.parentNode.getAttribute('id')
     obj = e.target.parentNode.parentNode
 
-    console.log('ID = ', id)
-    console.log('OBJ = ', obj)
-
     if (e.target.classList.contains('btn-feito')) {
-        console.log('botão Pronto')
 
+        // ATUALIZAR O TYPE PARA "FEITO"
         ConcluirTarefa(obj)
     }
     if (e.target.classList.contains('btn-voltar')) {
-        console.log('botão Voltar')
 
+        // ATUALIZAR O TYPE PARA "LISTA"
         RefazerTarefa(obj)
     }
 });
 
 document.querySelector('#done').addEventListener('click', function(e) {
-    id = e.target.parentNode.parentNode.getAttribute('id')
     obj = e.target.parentNode.parentNode
 
-    console.log('ID = ', id)
-    console.log('OBJ = ', obj)
-
     if (e.target.classList.contains('btn-voltar')) {
-        console.log('botão Refazer')
 
+        // ATUALIZAR O TYPE PARA "LISTA"
         FazerTarefa(obj)        
     }
     if (e.target.classList.contains('btn-cancelar')) {
-        console.log('botão Remover')
 
-        obj.remove()
+        // REMOVER O OBJETO DO BANCO DE DADOS
+        RemoverTarefa(obj)
     }
 });
