0839abd4f5698b0c57ef9135a4ee06ffa97fcc57
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-11-19 18:08:26 -0300

    Configurando as variáveis de ambiente para o serviço de e-mail do projeto

--- config.py
+++ config.py
@@ -6,6 +6,20 @@ class ProdConf(object):
     SECRET_KEY = config('SECRET_KEY', default="você-nunca-vai-adivinhar")
     SQLALCHEMY_DATABASE_URI = config('DATABASE_URI', default='sqlite:///')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = config('MAIL_SERVER', default='localhost')
+    MAIL_PORT = config('MAIL_PORT', cast=int, default='8025')
+    MAIL_USE_TLS = config('MAIL_USE_TLS', cast=bool, default=False)
+    MAIL_USERNAME = config('MAIL_USERNAME', default=None)
+    MAIL_PASSWORD = config('MAIL_PASSWORD', default=None)
+    MAIL_DEFAULT_SENDER = config(
+        'MAIL_DEFAULT_SENDER', 
+        default='contato@queirozt.webredirect.org'
+    )
+    ADMINS = config(
+        'ADMINS', 
+        cast=lambda v: [s.strip() for s in v.split(',')], 
+        default="email@exemplo.com,"
+    )
 
 
 class TestConf(object):
